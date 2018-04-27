1ecc194bfeb9755eae382e6ed802d633da276719
Jennifer Stoos <jstoos@bitlaw.com>
2018-04-27 19:11:08 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,8 +2,8 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_one :network
 
-  def build_network(network)
-    Network[self.network_id]
-  end
+  # def build_network(network)
+  #   Network[self.network_id]
+  # end
 
 end
